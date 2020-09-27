661f8db9fd416c04e5573a54f94387795d223cab
Guillermo López <Rigel.evolution@hotmail.com>
2020-09-27 16:33:56 -0500

    Add files via upload

--- sistemas_inteligentes/sistemas_inteligentes_2.R
+++ sistemas_inteligentes/sistemas_inteligentes_2.R
@@ -1,8 +1,19 @@
+-------------------------------------------------------------------------------
+  # Master of Science in Artificial Intelligence
+  # Authors: Guillermo López
+  # Date: 2020, September 27th
+------------------------------------------------------------------------------
+
 library(dplyr)
 library(ggplot2)
+library(caret)
 
-df<-read.csv("Downloads/sample_05.csv")
+# reading file from feature_engineering script
+db<-read.csv("sample_100.csv")
+# reading files from RapidMiner analysis
 df2<-read.csv("Downloads/sample_05_SOM.csv")
+df3<-read.csv("Downloads/sample_05_cluster.csv")
+# empty vector
 tots<-c()
 set.seed(1234)
 for(i in 1:10){
@@ -10,15 +21,31 @@ for(i in 1:10){
     kmeans(i)
   tots<-c(tots,cluster$tot.withinss)
 }
-plot(tots,type="l")
-
-df3<-read.csv("Downloads/sample_05_cluster.csv")
-df4<-group_by(SOM_0,SOM_1,cluster) %>%
-  summarize(freq=n())
-
-ggplot(df3,aes(x=SOM_0,y=SOM_1,color=cluster,size=freq)) + geom_point(alpha=0.7)
-
-df5<-merge(df,df3,by='row_id',sort=F) #%>%
+# elbow method plot to choose cluster's number
+plot(tots,type="b",xlab='Number of Clusters',
+     ylab='Total Sum of Squares Within Clusters',
+     main='Distance Evolution Within Clusters')
+abline(h=tots[5],lty=2)
+# clusters visualization tool
+df3 %>%
+  group_by(SOM_0,SOM_1,cluster) %>%
+  summarize(freq=n()) %>% 
+  ggplot(aes(x=SOM_0,y=SOM_1,color=cluster,size=freq)) + geom_point(alpha=0.7)
+# clusters characterization
+df4<-merge(df,df3,by='row_id',sort=F)
+df4 %>%
   group_by(cluster) %>%
-  summarize(viaje=mean(viaje))
-       
+  summarize(pro_viaje=mean(viaje),valor_promedio=mean(valor),total_valor=sum(valor),
+            tiempo_segundos_m1=mean(tiempo_segundos_m1),
+            misma_parada_m1=mean(misma_parada_m1),
+            misma_linea_m1=mean(misma_linea_m1),
+            mismo_vehiculo_m1=mean(mismo_vehiculo_m1),
+            distancia_kilometros_m1=mean(distancia_kilometros_m1,na.rm=T),
+            freq=n()) %>%
+  data.frame()
+# ensuring reproducibility
+set.seed(1234)
+expert_sample<-createDataPartition(df4$cluster,p=100/dim(df4)[1],list=F)
+#creating file to expert validation
+df5<-df4[expert_sample,] %>%
+  write.table("Downloads/expert_sample.csv",sep=",",row.names=F)
